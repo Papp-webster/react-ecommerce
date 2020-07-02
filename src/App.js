fc5b918532269eebe67098c8437f7eef07c52f1e
Papp László <papp.lazslo.web@gmail.com>
2020-07-02 22:07:48 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data";
+import "./App.css";
 
 function App() {
+  const openMenu = () => {
+    var element = document.querySelector(".sidebar");
+    element.classList.add("open");
+  };
+
+  const closeMenu = () => {
+    var element = document.querySelector(".sidebar");
+    element.classList.remove("open");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu}>&#9776;</button>
+          <a href="index.html">Ecommerce</a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html" className="cart">
+            Cart
+          </a>
+          <a href="signin.html">Signin</a>
+        </div>
       </header>
+      <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close" onClick={closeMenu}>
+          x
+        </button>
+        <ul>
+          <li>
+            <a href="index.html">Pants</a>
+          </li>
+          <li>
+            <a href="index.html">Shirts</a>
+          </li>
+        </ul>
+      </aside>
+      <main className="main">
+        <div className="content">
+          <ul className="products">
+            {data.products.map((product) => (
+              <li>
+                <div className="product">
+                  <img
+                    className="product-img"
+                    src={product.image}
+                    alt="product"
+                  />
+                  <div className="product-name">
+                    <a href="product.html">{product.name}</a>
+                  </div>
+                  <div className="product-brand">{product.brand}</div>
+                  <div className="product-price">{product.price}</div>
+                  <div className="product-rating">
+                    {product.rating} Stars({product.numReviews})
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
+      <footer className="footer">all rights reseverd.</footer>
     </div>
   );
 }
