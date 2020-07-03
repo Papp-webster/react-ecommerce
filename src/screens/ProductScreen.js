b1e531b9334a23728c30d2403df718566080e0c2
Papp László <papp.lazslo.web@gmail.com>
2020-07-03 09:58:13 +0200

    prodd

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,6 +1,31 @@
 import React from "react";
+import data from "../data";
+import { Link } from "react-router-dom";
 
 function ProductScreen(props) {
-  return <div>ProductScreen</div>;
+  console.log(props.match.params.id);
+  const product = data.products.find((x) => x.id === props.match.params.id);
+  return (
+    <div>
+      <div>
+        <Link to="/">Back to result</Link>
+      </div>
+      <div className="details">
+        <div className="details-image">
+          <img src={product.image} alt="product" />
+        </div>
+        <div className="details-info">
+          <ul>
+            <li>
+              <h4>{product.name}</h4>
+            </li>
+            <li>
+              {product.rating}Stars ({product.numReviews} Reviews)
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default ProductScreen;
